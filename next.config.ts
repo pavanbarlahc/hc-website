e459bcc3d97925e619a35df8277163ee782ab3f4
pavanbarlahc <pavan.barla@humancloud.co.in>
2025-08-29 14:09:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Recommended for development, can be adjusted for production
+  output: 'export', // Enables static HTML export
+  images: {
+    unoptimized: true, // Disables Next.js Image Optimization for static builds
+  },
 };
 
 export default nextConfig;
